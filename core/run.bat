a58adb4e1dd503f713b7c42df4586fbb5c4f8b2d
Andy Trimble <andrew.trimble@archarithms.com>
2014-02-19 10:24:16 -0700

    Converted into a multi module project.

--- core/run.bat
+++ core/run.bat
@@ -1,1 +1,1 @@
-java -javaagent:target/speakfast-1.0-SNAPSHOT.jar -Xmx2048m -Xms2048m -cp target/speakfast-1.0-SNAPSHOT.jar;config com.a2i.speak.Starter interactive
+java -Xmx2048m -Xms2048m -cp target/a2isim-core-1.0-SNAPSHOT.jar;config com.a2i.speak.Starter interactive
