e4dde99f0f9f0bd87414b91b18c6e85acf7edeaf
Andrew Trimble <andrew.trimble@archarithms.com>
2014-03-11 10:11:42 -0600

    Refactored the agent.

---
+++ agent/src/main/java/com/a2i/sim/core/FieldInfo.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2014 Archarithms Inc.
+ */
+
+package com.a2i.sim.core;
+
+/**
+ *
+ * @author atrimble
+ */
+public class FieldInfo {
+    private final String name;
+    private final String type;
+    private final String signature;
+
+    public FieldInfo(String name, String type, String signature) {
+        this.name = name;
+        this.type = type;
+        this.signature = signature;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + type + " : " + signature;
+    }
+}
