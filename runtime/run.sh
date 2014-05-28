0ad1eaeba48b2a52ea97a30a9a573595c8007174
Andrew Trimble <andrew.trimble@archarithms.com>
2014-05-28 15:36:54 -0600

    Changed the version to 1.0.1-SNAPSHOT.

--- runtime/run.sh
+++ runtime/run.sh
@@ -1,3 +1,3 @@
-#/bin/bash
+#!/bin/bash
 
-java -javaagent:bin/bigio-agent-1.0-SNAPSHOT.jar -cp bin/bigio-core-1.0-SNAPSHOT.jar:config:lib/*:components/* io.bigio.Starter
+java -javaagent:bin/bigio-agent-1.0.1-SNAPSHOT.jar -cp bin/bigio-core-1.0.1-SNAPSHOT.jar:config:lib/*:components/* io.bigio.Starter
