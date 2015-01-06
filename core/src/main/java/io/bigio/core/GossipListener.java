66475fc8dcced38defe2fe66984daf53190c1d24
Andrew Trimble <andrew.trimble@archarithms.com>
2015-01-05 17:27:27 -0700

    Updated copyright header.

--- core/src/main/java/io/bigio/core/GossipListener.java
+++ core/src/main/java/io/bigio/core/GossipListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Archarithms Inc.
+ * Copyright (c) 2015, Archarithms Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
