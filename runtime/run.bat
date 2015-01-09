9f44fa210e2a016d704fc9ddaef4b69846e6ae0f
Andrew Trimble <andrew.trimble@arcarithms.com>
2015-01-09 13:19:39 -0500

    Preparing for 1.1 release.

--- runtime/run.bat
+++ runtime/run.bat
@@ -1,1 +1,1 @@
-java -javaagent:bin/bigio-agent-1.1-SNAPSHOT.jar -Xmx2048m -Xms2048m -cp "bin/bigio-core-1.1-SNAPSHOT.jar;config;lib/*;components/*" io.bigio.Starter
+java -javaagent:bin/bigio-agent-1.1.jar -Xmx2048m -Xms2048m -cp "bin/bigio-core-1.1.jar;config;lib/*;components/*" io.bigio.Starter
--- runtime/run.sh
+++ runtime/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -javaagent:bin/bigio-agent-1.1-SNAPSHOT.jar -cp bin/bigio-core-1.1-SNAPSHOT.jar:config:lib/*:components/* io.bigio.Starter
+java -javaagent:bin/bigio-agent-1.1-cp bin/bigio-core-1.1:config:lib/*:components/* io.bigio.Starter
