f573c494aead3d05433546a4af464ac1f7276c86
Andrew Trimble <andrew.trimble@archarithms.com>
2015-04-28 15:06:39 -0500

    Updated version in run scripts.

--- runtime/run.sh
+++ runtime/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -javaagent:bin/bigio-agent-1.1.1-SNAPSHOT-cp bin/bigio-core-1.1.1-SNAPSHOT:config:lib/*:components/* io.bigio.Starter
+java -javaagent:bin/bigio-agent-1.1.2-SNAPSHOT-cp bin/bigio-core-1.1.2-SNAPSHOT:config:lib/*:components/* io.bigio.Starter
