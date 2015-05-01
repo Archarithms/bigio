3934296df393747e2bfe31c205d1582aaedc37ac
Andrew Trimble <andrew.trimble@arcarithms.com>
2015-05-01 12:52:41 -0400

    Updating run script versions for release.

--- runtime/run.bat
+++ runtime/run.bat
@@ -1,1 +1,1 @@
-java -javaagent:bin/bigio-agent-1.1.2.jar -Xmx2048m -Xms2048m -cp "bin/bigio-core-1.1.2.jar;config;lib/*;components/*" io.bigio.Starter
+java -javaagent:bin/bigio-agent-1.1.3.jar -Xmx2048m -Xms2048m -cp "bin/bigio-core-1.1.3.jar;config;lib/*;components/*" io.bigio.Starter
